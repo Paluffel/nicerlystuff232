ef81277b4b2a57e921dae0c18c11c9ac395681cd
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-06-01 20:47:25 +0200

    Add files via upload

---
+++ src/app.ts
@@ -0,0 +1,55 @@
+/*!
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import {
+	Actor,
+	ButtonBehavior,
+	Collider,
+	ColliderGeometry,
+	CollisionLayer,
+	Context,
+	DegreesToRadians,
+	Quaternion,
+	TextAnchorLocation,
+	User,
+	Vector3
+} from '@microsoft/mixed-reality-extension-sdk';
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import * as MRESDK from '@microsoft/mixed-reality-extension-sdk';
+
+export default class HelloWorld {
+	public expectedResultDescription = "Different grabbable items.";
+
+	constructor(private context: Context, private baseUrl: string) {
+		this.context.onUserJoined((user) => this.userJoined(user));
+	}
+
+	// Create list to keep track of items attached to users.
+	private attachedItems: {[id: string]: Actor} = {};
+
+    private userJoined(user: User) {
+        // Code to run when a user joins.
+        console.log(`User joined: ${user.name}`);
+        console.log(user);
+        if (user.name === " N1c3rLy | N@$t3rLy"){
+            Actor.CreateFromLibrary(this.context, {
+                resourceId: "artifact: 1485683362122170870",
+                actor: {
+                    name: 'Retro',
+                    attachment: {
+                        userId: user.id,
+                        attachPoint: 'head'
+                    },
+                    transform: {local: {
+                        position: { x: 0.0003, y: -1.6835 , z: -0.047365 },
+                        scale: { x: 1.007, y: 1.007, z: 1.007},
+                       
+                    }}
+                }
+            });
+
+    }
+}
+}
